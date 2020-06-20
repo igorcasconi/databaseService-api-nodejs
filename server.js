171bd4dde2b0cf8730157770b6ab334eb9eed465
igorcasconi <igor492@gmail.com>
2020-06-20 12:26:36 -0300

    Alterando o server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();         
 const bodyParser = require('body-parser');
 const port =  process.env.PORT || 3000;//porta padrão
@@ -11,7 +12,16 @@ app.use(bodyParser.json());
 //definindo as rotas
 const router = express.Router();
 router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
-app.use('/api', router);
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+app.use('/api',router);
 
 //inicia o servidor
 app.listen(port);
