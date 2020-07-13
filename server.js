5482c9b9af04f30e5ac8f99870bd123429ec9100
igorcasconi <igor492@gmail.com>
2020-07-13 18:10:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -58,8 +58,12 @@ router.post('/insert-mov/:id/:type', (req, res) =>{
   execSQLQuery('INSERT INTO Movimentacao_Caixa (Movimentacao_Caixa_product,Movimentacao_Caixa_value,Movimentacao_Caixa_date, \
   Movimentacao_Caixa_userFirebase,Movimentacao_Caixa_Tipo_Movimentacao_id,Movimentacao_Caixa_Paymode) VALUES ("' + product + '",' + value + ',' +
   'CAST(CONCAT(DATE_FORMAT(STR_TO_DATE("' + date +'", "%d/%m/%Y"),"%Y-%m-%d"), " ' + time + '") AS DATETIME),"' + req.params.id + '","' + req.params.type + '","' + paymode + '");', res);
-
-  execSQLQuery('UPDATE Caixa_Saldo SET Caixa_Saldo_value = Caixa_Saldo_value + ' + value + ' WHERE Caixa_Saldo_userFirebase = "' + req.params.id + '";', res);
+  
+  if( type === 1) {
+    execSQLQuery('UPDATE Caixa_Saldo SET Caixa_Saldo_value = Caixa_Saldo_value + ' + value + ' WHERE Caixa_Saldo_userFirebase = "' + req.params.id + '";', res);
+  } else {
+    execSQLQuery('UPDATE Caixa_Saldo SET Caixa_Saldo_value = Caixa_Saldo_value - ' + value + ' WHERE Caixa_Saldo_userFirebase = "' + req.params.id + '";', res);
+  }
 })
 
 router.get('/movs/:id/:type', (req, res) =>{
@@ -68,3 +72,18 @@ router.get('/movs/:id/:type', (req, res) =>{
     execSQLQuery('SELECT *, DATE_FORMAT(Movimentacao_Caixa_date, "%d/%m/%Y") as data_formatada, DATE_FORMAT(Movimentacao_Caixa_date, "%H:%i") as hora_formatada \
      FROM Movimentacao_Caixa' + filter + ' ORDER BY Movimentacao_Caixa_date DESC', res);
 })
+
+router.get('/movs-year/:id', (req, res) =>{
+  let filter = '';
+  if(req.params.id) filter = ' WHERE a.Movimentacao_Caixa_userFirebase="' + req.params.id + '"';
+  execSQLQuery('SELECT YEAR(a.Movimentacao_Caixa_date) AS "mes", SUM(a.Movimentacao_Caixa_value) AS "soma" FROM Movimentacao_Caixa AS a ' + filter + ' GROUP BY YEAR(Movimentacao_Caixa_date) \
+  ORDER BY YEAR(Movimentacao_Caixa_date) DESC', res);
+})
+
+router.get('/movs-month/:id', (req, res) =>{
+  let filter = '';
+  if(req.params.id) filter = ' WHERE a.Movimentacao_Caixa_userFirebase="' + req.params.id + '"';
+  execSQLQuery('SELECT a.Movimentacao_Caixa_id, MONTHNAME(a.Movimentacao_Caixa_date) AS "mes",YEAR(a.Movimentacao_Caixa_date) AS "ano", SUM(a.Movimentacao_Caixa_value) AS "soma" \
+  FROM Movimentacao_Caixa AS a ' + filter + ' GROUP BY MONTHNAME(a.Movimentacao_Caixa_date), YEAR(Movimentacao_Caixa_date) ORDER BY a.Movimentacao_Caixa_date DESC', res);
+})
+
