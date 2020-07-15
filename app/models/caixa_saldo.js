c0ad9ba06710ac22f88dac408a95d4b52cca63e7
igorcasconi <igor492@gmail.com>
2020-07-15 01:27:10 -0300

    Update project caixa_saldo.js

--- app/models/caixa_saldo.js
+++ app/models/caixa_saldo.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     const Caixa_Saldo = sequelize.define('Caixa_Saldo', {
-      Caixa_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      Caixa_Saldo_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
       Caixa_Saldo_value: DataTypes.DOUBLE ,
       Caixa_Saldo_userFirebase: DataTypes.STRING,
     });
