9e2e9289732742f1c0c8a1749617fc8a8c816062
igorcasconi <igor492@gmail.com>
2020-07-15 14:55:34 -0300

    Update function strToDate

--- config/strToDate.js
+++ config/strToDate.js
@@ -1,6 +1,4 @@
-import module from "@react-native-firebase/app";
-
-module.exports = (dateStr: any) => {
+module.exports = (dateStr) => {
     var parts = dateStr.split("/");
     return new Date(parts[2], parts[1] - 1, parts[0]);
 }
